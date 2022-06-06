0f934fce8444a4b5916f1c68741b13b2faa3d860
Pablo Castaneda <pabloc.blog@gmail.com>
2022-06-06 01:25:17 -0600

    primer commit

---
+++ Fortigate-SCP-Backup/Forti-SCP.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Individual Fortigate settings 
+HOST="x.x.x.x"						# Fortigate hostname of IP (must be accessible via ssh)
+USER="admin"									# Fortigate username
+PASS="password"								# Fortigate password
+						
+
+
+
+NOW=$(date +"_%m-%d-%Y")
+CMD=$@
+
+echo "Fortigate backup for $FW"
+echo "By Jeremy Jenkins (jeremyjenkins5 (at) gmail (dot) com)"
+echo "beginning backup...."
+
+SCRIPT=$(expect -c "
+		spawn scp -o StrictHostKeyChecking=no $USER@$HOST:sys_config . $CMD
+		match_max 100000
+		expect \"*?assword:*\"
+		send -- \"$PASS\r\"
+		expect \"*?#*\"
+		send -- \"sys_config /root\"
+		send -- \"\r\"
+		expect eof
+")
+
+echo "$SCRIPT"
+echo "script finished. note any errors above"
